cbfab1e1aff9494ebe5e05ceef3dcf7f0b7d5d51
walton.wang <walton.wang@sap.com>
2015-06-19 16:12:03 +0800

    i071628 JIRA: BEB- Description: add $cordovaPush ability into project

--- app/main/main.js
+++ app/main/main.js
@@ -19,4 +19,55 @@ angular.module('main', [
       controller: 'StartCtrl as start'
     });
   // TODO: do your thing
+})
+.run(function ($ionicPlatform, $cordovaPush, $rootScope) {
+  console.log(1111);
+
+  $ionicPlatform.ready(function () {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+
+    if (window.StatusBar) {
+      // Set the statusbar to use the default style, tweak this to
+      // remove the status bar on iOS or change it to use white instead of dark colors.
+      StatusBar.styleDefault();
+    }
+
+    var iosConfig = {
+      'badge': true,
+      'sound': true,
+      'alert': true,
+    };
+    $cordovaPush.register(iosConfig).then(function (result) {
+      // Success -- send deviceToken to server, and store for future use
+      console.log('deviceToken: ' + result);
+      $rootScope.deviceToken = result;
+      //$http.post('http://server.co/', {user: 'Bob', tokenID: result.deviceToken})
+    }, function (err) {
+      alert('Registration error: ' + err);
+    });
+  });
+
+  $rootScope.$on('$cordovaPush:notificationReceived', function (event, notification) {
+    console.log('notificationReceived:', notification);
+    if (notification.alert) {
+      navigator.notification.alert(notification.alert);
+    }
+
+    if (notification.sound) {
+      var snd = new Media(event.sound);
+      snd.play();
+    }
+
+    if (notification.badge) {
+      $cordovaPush.setBadgeNumber(notification.badge).then(function (result) {
+        // Success!
+        console.log(result);
+      }, function (err) {
+        // An error occurred. Show a message to the user
+        console.error(err);
+      });
+    }
+  });
 });
